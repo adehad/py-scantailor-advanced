84c826ba26af9a35a59c9479ac750c5b5d510c64
adehad <26027314+adehad@users.noreply.github.com>
2025-12-24 16:41:53 +0000

    fixup! port About Dialog
    Move AboutDialog to dialogs/ folder with co-located .ui file

--- src/scantailor/app/ui/dialogs/__init__.py
+++ src/scantailor/app/ui/dialogs/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from scantailor.app.ui.dialogs.about import AboutDialog
 from scantailor.app.ui.dialogs.fix_dpi_dialog import FixDpiDialog
 from scantailor.app.ui.dialogs.load_files_status_dialog import LoadFilesStatusDialog
 from scantailor.app.ui.dialogs.new_open_project_panel import NewOpenProjectPanel
@@ -11,6 +12,7 @@ from scantailor.app.ui.dialogs.settings_dialog import SettingsDialog
 from scantailor.app.ui.dialogs.status_bar_panel import StatusBarPanel
 
 __all__ = [
+    "AboutDialog",
     "FixDpiDialog",
     "LoadFilesStatusDialog",
     "NewOpenProjectPanel",
