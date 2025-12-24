cdca282f52a50fcab574d6112e64a08bf1903a16
adehad <26027314+adehad@users.noreply.github.com>
2025-12-24 16:48:22 +0000

    fixup! Add SettingsDialog and FixDpiDialog
    Rename dialog files to remove _dialog suffix

--- src/scantailor/app/ui/dialogs/__init__.py
+++ src/scantailor/app/ui/dialogs/__init__.py
@@ -3,12 +3,12 @@
 from __future__ import annotations
 
 from scantailor.app.ui.dialogs.about import AboutDialog
-from scantailor.app.ui.dialogs.fix_dpi_dialog import FixDpiDialog
-from scantailor.app.ui.dialogs.load_files_status_dialog import LoadFilesStatusDialog
+from scantailor.app.ui.dialogs.fix_dpi import FixDpiDialog
+from scantailor.app.ui.dialogs.load_files_status import LoadFilesStatusDialog
 from scantailor.app.ui.dialogs.new_open_project_panel import NewOpenProjectPanel
-from scantailor.app.ui.dialogs.project_files_dialog import ProjectFilesDialog
-from scantailor.app.ui.dialogs.remove_pages_dialog import RemovePagesDialog
-from scantailor.app.ui.dialogs.settings_dialog import SettingsDialog
+from scantailor.app.ui.dialogs.project_files import ProjectFilesDialog
+from scantailor.app.ui.dialogs.remove_pages import RemovePagesDialog
+from scantailor.app.ui.dialogs.settings import SettingsDialog
 from scantailor.app.ui.dialogs.status_bar_panel import StatusBarPanel
 
 __all__ = [
